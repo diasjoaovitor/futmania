6581ec17a0450741448ac924e41886a0b2cdd848
João Vitor <jvitordiass@outlook.com.br>
2023-12-15 15:01:08 -0300

    feat: useQueriesMembersAndBabas

---
+++ src/shared/react-query/queries/useQueriesMembersAndBabas.ts
@@ -0,0 +1,42 @@
+import { useQueries } from '@tanstack/react-query'
+import { getBabas, getMembers } from '@/shared/firebase'
+
+export function useQueriesMembersAndBabas(id: string) {
+  const [
+    {
+      data: membersData,
+      isPending: membersIsPending,
+      isError: isMembersError,
+      error: membersError
+    },
+    {
+      data: babasData,
+      isPending: babasIsPending,
+      isError: isBabasError,
+      error: babasError
+    }
+  ] = useQueries({
+    queries: [
+      {
+        queryKey: ['members'],
+        queryFn: async () => await getMembers(id)
+      },
+      {
+        queryKey: ['babas'],
+        queryFn: async () => await getBabas(id)
+      }
+    ]
+  })
+
+  membersError && console.error({ membersError })
+  babasError && console.error({ babasError })
+
+  return {
+    membersData,
+    membersIsPending,
+    isMembersError,
+    babasData,
+    babasIsPending,
+    isBabasError
+  }
+}
