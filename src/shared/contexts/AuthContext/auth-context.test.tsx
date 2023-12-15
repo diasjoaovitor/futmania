48d5087b794929195fa4da40eb00d7bf93c2f739
João Vitor <jvitordiass@outlook.com.br>
2023-12-15 14:54:14 -0300

    test: AuthContext

--- src/shared/contexts/AuthContext/auth-context.test.tsx
+++ src/shared/contexts/AuthContext/auth-context.test.tsx
@@ -1,52 +1,55 @@
-import { render } from '@testing-library/react'
-import { RouterProvider, createMemoryRouter } from 'react-router-dom'
-import { AuthProvider, PrivateRoute } from '.'
+import { render, renderHook, screen, waitFor } from '@testing-library/react'
+import { useQueryUser } from '@/shared/react-query'
+import { TBabaUser } from '@/shared/types'
 import { useAuth } from './useAuth'
+import { AuthProvider } from '.'
 
-jest.mock('./useAuth.ts')
+jest.mock('../../react-query/queries/useQueryUser')
 
-const mockedUseAuth = useAuth as jest.Mock<{
-  isLoading: boolean
-  user: null | string
-}>
-
-function memoryRouter() {
-  const router = createMemoryRouter(
-    [
-      {
-        path: '/',
-        element: (
-          <AuthProvider>
-            <PrivateRoute />
-          </AuthProvider>
-        )
-      },
-      { path: '/entrar', element: <AuthProvider /> }
-    ],
-    {
-      initialEntries: ['/']
-    }
-  )
-  render(<RouterProvider router={router} />)
-  return router
+type TUseQueryUser = {
+  data: TBabaUser | undefined
+  isError: boolean
+  isPending: boolean
 }
 
-describe('<ThemeProvider />', () => {
-  it('should redirect to authentication page', () => {
-    mockedUseAuth.mockImplementation(() => ({
-      isLoading: false,
-      user: null
-    }))
-    const router = memoryRouter()
-    expect(router.state.location.pathname).toBe('/entrar')
+const mockedUseQueryUser = useQueryUser as unknown as jest.Mock<TUseQueryUser>
+
+function mockedUseUserSetup(args: TUseQueryUser | object) {
+  const state: TUseQueryUser = {
+    isPending: false,
+    data: {
+      id: '123',
+      name: 'Baba'
+    },
+    isError: false
+  }
+  mockedUseQueryUser.mockImplementation(() => ({
+    ...state,
+    ...args
+  }))
+}
+
+describe('<AuthProvider />', () => {
+  it('should render error message', async () => {
+    mockedUseUserSetup({ data: undefined, isError: true })
+    render(<AuthProvider />)
+    await waitFor(() => {
+      expect(
+        screen.getByText('Não foi possível obter o usuário')
+      ).toBeInTheDocument()
+    })
   })
 
-  it('should access private route', () => {
-    mockedUseAuth.mockImplementation(() => ({
-      isLoading: false,
-      user: 'user'
-    }))
-    const router = memoryRouter()
-    expect(router.state.location.pathname).toBe('/')
+  it('should return default baba user', async () => {
+    mockedUseUserSetup({})
+    const {
+      result: {
+        current: { user, babaUser }
+      }
+    } = renderHook(() => useAuth())
+    expect(user).toBe(null)
+    await waitFor(() => {
+      expect(babaUser).toEqual({ id: '123', name: 'Baba' })
+    })
   })
 })
