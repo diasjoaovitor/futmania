248b0b025710d69f3fe75b49cf5d8d8b12011576
João Vitor <jvitordiass@outlook.com.br>
2023-12-15 14:56:45 -0300

    refactor: separate function to get value and checked attributes

--- src/shared/functions/getters/index.ts
+++ src/shared/functions/getters/index.ts
@@ -1,11 +1,27 @@
+function getElements(
+  e: React.FormEvent<HTMLFormElement>,
+  elementNames: string[]
+) {
+  return elementNames.map((elementName) => {
+    const element = e.currentTarget.elements.namedItem(elementName)
+    return element
+  })
+}
+
 export function getElementValues(
   e: React.FormEvent<HTMLFormElement>,
-  elements: string[]
-): string[] {
-  return elements.map((elementName) => {
-    const element = e.currentTarget.elements.namedItem(
-      elementName
-    ) as HTMLInputElement | null
-    return element ? element.value : ''
-  })
+  elementNames: string[]
+) {
+  const elements = getElements(e, elementNames)
+  return elements.map((element) => (element as HTMLInputElement | null)?.value)
+}
+
+export function getElementsCheckedValues(
+  e: React.FormEvent<HTMLFormElement>,
+  elementNames: string[]
+) {
+  const elements = getElements(e, elementNames)
+  return elements.map(
+    (element) => (element as HTMLInputElement | null)?.checked
+  )
 }
