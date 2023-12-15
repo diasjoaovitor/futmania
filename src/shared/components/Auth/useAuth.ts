51d508185f4afa57e06083f57d0bcdbe7ac1d44f
João Vitor <jvitordiass@outlook.com.br>
2023-12-15 14:41:42 -0300

    fix: navigate to exact route entrar

--- src/shared/components/Auth/useAuth.ts
+++ src/shared/components/Auth/useAuth.ts
@@ -5,7 +5,7 @@ export function useAuth() {
   const navigate = useNavigate()
 
   const handleLogin = () => {
-    navigate('entrar')
+    navigate('/entrar')
   }
 
   const handleLogout = async () => {
