307afd39762c93bdbd7efb830922d8aa8e97b4c9
João Vitor <jvitordiass@outlook.com.br>
2024-01-03 15:09:10 -0300

    feat: add months and currentDate

--- src/shared/states/date.ts
+++ src/shared/states/date.ts
@@ -1,3 +1,7 @@
 import dayjs from 'dayjs'
 
 export const createdAt = dayjs().toISOString()
+
+export const months = new Array(12).fill(0).map((_, i) => i)
+
+export const currentDate = dayjs().format('YYYY/MM/DD')
