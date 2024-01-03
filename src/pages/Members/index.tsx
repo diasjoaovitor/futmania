78fe0da3a7b15bc5024677bbeba01a5fd53b37a2
João Vitor <jvitordiass@outlook.com.br>
2024-01-03 00:03:48 -0300

    refactor: move user to useMembers and remove box wrapper and style

--- src/pages/Members/index.tsx
+++ src/pages/Members/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import {
   Dialog,
   ExpandButton,
@@ -6,19 +6,18 @@ import {
   MembersForm,
   MembersList
 } from '@/shared/components'
-import { useAuthContext, useThemeContext } from '@/shared/contexts'
+import { useThemeContext } from '@/shared/contexts'
 import { useLimit } from '@/shared/hooks'
-import { separateMembers, sortMembers } from './functions'
+import { separateMembers, sortMembersByName } from '@/shared/functions'
 import { useMembers } from './useMembers'
-import * as S from './style'
 
 export function Members() {
-  const { user } = useAuthContext()
   const {
     theme: { palette }
   } = useThemeContext()
 
   const {
+    user,
     membersFormProps,
     handleOpenModal,
     handleOpenModalUpdate,
@@ -31,59 +30,57 @@ export function Members() {
   const { fixedMembers, goalkeepers, nonMembers } = separateMembers(members)
 
   const {
-    array: limitedNonMembers,
+    limited: limitedNonMembers,
     isFull,
     handleLimit
-  } = useLimit(sortMembers(nonMembers))
+  } = useLimit(sortMembersByName(nonMembers), 5)
 
   return (
-    <Box sx={S.Wrapper}>
-      <Layout title="Membros" isPending={isPending} alertProps={alertProps}>
-        {members.length !== 0 ? (
-          <>
-            {fixedMembers.length !== 0 && (
+    <Layout title="Membros" isPending={isPending} alertProps={alertProps}>
+      {members.length !== 0 ? (
+        <>
+          {fixedMembers.length !== 0 && (
+            <MembersList
+              title="Membros Fixos"
+              members={sortMembersByName(fixedMembers)}
+              color={palette.primary.main}
+              handleClick={handleOpenModalUpdate}
+            />
+          )}
+          {goalkeepers.length !== 0 && (
+            <MembersList
+              title="Goleiros"
+              members={sortMembersByName(goalkeepers)}
+              color={palette.secondary.main}
+              handleClick={handleOpenModalUpdate}
+            />
+          )}
+          {nonMembers.length !== 0 && (
+            <>
               <MembersList
-                title="Membros Fixos"
-                members={sortMembers(fixedMembers)}
-                color={palette.primary.main}
+                title="Membros Avulsos"
+                members={limitedNonMembers}
+                color={palette.secondary.light}
                 handleClick={handleOpenModalUpdate}
               />
-            )}
-            {goalkeepers.length !== 0 && (
-              <MembersList
-                title="Goleiros"
-                members={sortMembers(goalkeepers)}
-                color={palette.secondary.main}
-                handleClick={handleOpenModalUpdate}
-              />
-            )}
-            {nonMembers.length !== 0 && (
-              <>
-                <MembersList
-                  title="Membros Avulsos"
-                  members={limitedNonMembers}
-                  color={palette.secondary.light}
-                  handleClick={handleOpenModalUpdate}
-                />
-                {nonMembers.length > 5 && (
-                  <ExpandButton isExpanded={isFull} handleClick={handleLimit} />
-                )}
-              </>
-            )}
-          </>
-        ) : (
-          <Typography>Não há membros cadastrados</Typography>
-        )}
-        {user && (
-          <>
-            <Button sx={{ my: 2 }} variant="outlined" onClick={handleOpenModal}>
-              Cadastrar Membros
-            </Button>
-            <MembersForm {...membersFormProps} />
-            <Dialog {...dialogProps} />
-          </>
-        )}
-      </Layout>
-    </Box>
+              {nonMembers.length > 5 && (
+                <ExpandButton isExpanded={isFull} handleClick={handleLimit} />
+              )}
+            </>
+          )}
+        </>
+      ) : (
+        <Typography>Não há membros cadastrados</Typography>
+      )}
+      {user && (
+        <>
+          <Button sx={{ my: 2 }} variant="outlined" onClick={handleOpenModal}>
+            Cadastrar Membros
+          </Button>
+          <MembersForm {...membersFormProps} />
+          <Dialog {...dialogProps} />
+        </>
+      )}
+    </Layout>
   )
 }
