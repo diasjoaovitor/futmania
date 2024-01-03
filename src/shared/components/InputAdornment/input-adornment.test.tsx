aec8fda46d6fdc1d5dc90a879421ea9bb4fd5ac9
João Vitor <jvitordiass@outlook.com.br>
2024-01-03 00:28:34 -0300

    test: InputAdornment

---
+++ src/shared/components/InputAdornment/input-adornment.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ThemeProvider } from '@/shared/contexts'
+import { InputAdornment } from '.'
+
+describe('<InputAdornment />', () => {
+  it('should render component', async () => {
+    const handleChange = jest.fn()
+    render(
+      <ThemeProvider>
+        <InputAdornment
+          label="Input Adornment"
+          name="name"
+          value={2}
+          color="error"
+          handleChange={handleChange}
+        />
+      </ThemeProvider>
+    )
+    const input = screen.getByLabelText('Input Adornment *')
+    expect(input).toBeInTheDocument()
+    await userEvent.type(input, '5')
+    expect(handleChange).toHaveBeenCalled()
+  })
+})
