2cc31e1faecc0ff06facb33cadeb2bd7af3ea502
João Vitor <jvitordiass@outlook.com.br>
2024-01-03 00:26:37 -0300

    feat: export new queries and mutations

--- src/shared/react-query/index.ts
+++ src/shared/react-query/index.ts
@@ -1,5 +1,13 @@
+export * from './mutations/useMutationCreateFinance'
+export * from './mutations/useMutationCreateFinances'
+export * from './mutations/useMutationDeleteFinance'
+export * from './mutations/useMutationUpdateFinance'
+
 export * from './mutations/useMutationCreateMember'
 export * from './mutations/useMutationDeleteMember'
 export * from './mutations/useMutationUpdateMember'
+
+export * from './queries/useQueryMembers'
+export * from './queries/useQueryFinances'
 export * from './queries/useQueriesMembersAndBabas'
 export * from './queries/useQueryUser'
