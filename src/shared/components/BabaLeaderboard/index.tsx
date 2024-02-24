b303924c729197a39d2121b33f11574c1a4934cf
João Vitor <jvitordiass@outlook.com.br>
2024-02-24 16:12:56 -0300

    feat: baba leaderboard

---
+++ src/shared/components/BabaLeaderboard/index.tsx
@@ -0,0 +1,53 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material'
+import { TBaba, TMember } from '@/shared/types'
+import { sortTeamsByScore } from './functions'
+import { BabaLeaderboardRow } from '..'
+
+const cols = ['Classificação', 'PTS', 'V', 'E']
+
+type Props = {
+  baba: TBaba
+  members: TMember[]
+  handleClick(member: TMember): void
+}
+
+export function BabaLeaderboard({
+  baba,
+  members: allMembers,
+  handleClick
+}: Props) {
+  const { teams } = baba
+  return (
+    <TableContainer component={Paper} sx={{ my: 3 }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {cols.map((col, index) => (
+              <TableCell key={index} align="center">
+                {col}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sortTeamsByScore([...teams]).map((team) => (
+            <BabaLeaderboardRow
+              key={team.name}
+              team={team}
+              members={allMembers}
+              handleClick={handleClick}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
