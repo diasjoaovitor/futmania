6b4ad1d15ddfc06822fcc5b51c57aa203097d4f2
João Vitor <jvitordiass@outlook.com.br>
2024-02-24 14:42:51 -0300

    feat: stats

--- src/pages/Stats/index.tsx
+++ src/pages/Stats/index.tsx
@@ -1,16 +1,50 @@
 import { Box, Typography } from '@mui/material'
-import { AlertProps, Layout } from '@/shared/components'
+import {
+  Layout,
+  MemberModal,
+  MembersRanking,
+  SelectSeason
+} from '@/shared/components'
+import { useStats } from './useStats'
 import * as S from './style'
 
 export function Stats() {
+  const {
+    season,
+    year,
+    years,
+    statsInSeason,
+    handleOpenMemberModal,
+    memberModalProps,
+    handlePeriodChange,
+    alertProps,
+    isPending
+  } = useStats()
+
   return (
     <Box sx={S.Wrapper}>
       <Layout
         title="Estatísticas"
-        alertProps={{} as AlertProps}
-        isPending={false}
+        isPending={isPending}
+        alertProps={alertProps}
       >
-        <Typography>Conteúdo de estatísticas</Typography>
+        <SelectSeason
+          season={season}
+          year={year}
+          years={years}
+          handleChange={handlePeriodChange}
+        />
+        {statsInSeason.length > 0 ? (
+          <>
+            <MembersRanking
+              stats={statsInSeason}
+              handleClick={handleOpenMemberModal}
+            />
+            <MemberModal {...memberModalProps} />
+          </>
+        ) : (
+          <Typography my={2}>Não há babas nessa temporada</Typography>
+        )}
       </Layout>
     </Box>
   )
--- src/pages/Stats/stats.test.tsx
+++ src/pages/Stats/stats.test.tsx
@@ -1,14 +1,53 @@
 import { render, screen } from '@testing-library/react'
+import { AuthProvider } from '@/shared/contexts'
+import { useAuth as useAuthContext } from '@/shared/contexts/AuthContext/useAuth'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { TBabaUser } from '@/shared/types'
 import { Stats } from '.'
 
+jest.mock('../../shared/contexts/AuthContext/useAuth')
+
+const client = new QueryClient()
+
+type TUseAuthContext = {
+  user: null
+  babaUser: TBabaUser
+  isLoading: boolean
+}
+
+const mockedUseAuthContext =
+  useAuthContext as unknown as jest.Mock<TUseAuthContext>
+
+function mockedUseAuthContextSetup(args: TUseAuthContext | object) {
+  const state: TUseAuthContext = {
+    isLoading: false,
+    user: null,
+    babaUser: {
+      id: '123',
+      name: 'Baba'
+    }
+  }
+  mockedUseAuthContext.mockImplementation(() => ({
+    ...state,
+    ...args
+  }))
+}
+
 describe('<Stats />', () => {
+  beforeEach(() => {
+    mockedUseAuthContextSetup({})
+  })
+
   it('should render the heading and main', () => {
-    render(<Stats />)
-    expect(
-      screen.getByRole('heading', { name: /estatísticas/i })
-    ).toBeInTheDocument()
-    expect(screen.getByRole('main').firstChild!.textContent).toBe(
-      'Conteúdo de estatísticas'
+    render(
+      <QueryClientProvider client={client}>
+        <AuthProvider>
+          <Stats />
+        </AuthProvider>
+      </QueryClientProvider>
     )
+    expect(
+      screen.getByRole('heading', { name: 'Estatísticas', level: 1 })
+    ).toBeInTheDocument()
   })
 })
