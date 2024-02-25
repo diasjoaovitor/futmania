705926d0d34313b6390d926d04e4ffba7960b26b
João Vitor <jvitordiass@outlook.com.br>
2024-02-25 07:57:30 -0300

    feat: add mutations

---
+++ src/shared/react-query/mutations/useMutationDeleteBaba.ts
@@ -0,0 +1,16 @@
+import { useMutation } from '@tanstack/react-query'
+import { deleteBaba } from '@/shared/firebase'
+
+export function useMutationDeleteBaba() {
+  const { mutate, data, isPending, isError, error } = useMutation({
+    mutationKey: ['deleteBaba'],
+    mutationFn: async (id: string) => {
+      await deleteBaba(id)
+      return id
+    }
+  })
+
+  error && console.error({ error })
+
+  return { mutate, data, isPending, isError }
+}
