2b8daa09447d3b8f650f1b4bb42c431065a0cb57
João Vitor <jvitordiass@outlook.com.br>
2024-02-25 08:27:00 -0300

    fix: add handleOpenMemberStats prop

--- src/shared/components/MembersForm/members-form.test.tsx
+++ src/shared/components/MembersForm/members-form.test.tsx
@@ -7,11 +7,13 @@ describe('<MembersForm />', () => {
     const handleClose = jest.fn()
     const handleDelete = jest.fn()
     const handleSubmit = jest.fn()
+    const handleOpenMemberStats = jest.fn()
     const { rerender } = render(
       <MembersForm
         title="MembersForm"
         isOpened={true}
         member={{} as TMember}
+        handleOpenMemberStats={handleOpenMemberStats}
         handleClose={handleClose}
         handleDelete={handleDelete}
         handleSubmit={handleSubmit}
@@ -35,6 +37,7 @@ describe('<MembersForm />', () => {
             id: '1'
           } as TMember
         }
+        handleOpenMemberStats={handleOpenMemberStats}
         handleClose={handleClose}
         handleDelete={handleDelete}
         handleSubmit={handleSubmit}
