1dc7cf2cae73a711ee606fdbdb3662567a3ff83e
João Vitor <jvitordiass@outlook.com.br>
2024-02-25 07:58:40 -0300

    feat: add seasons state

--- src/shared/states/date.ts
+++ src/shared/states/date.ts
@@ -5,3 +5,13 @@ export const createdAt = dayjs().toISOString()
 export const months = new Array(12).fill(0).map((_, i) => i)
 
 export const currentDate = dayjs().format('YYYY/MM/DD')
+
+export const seasons = ['0-3', '4-7', '8-11']
+
+export const currentSeason = seasons.find((season) => {
+  const [monthFrom] = season.split('-')
+  return new Array(4)
+    .fill(-1)
+    .map(() => Number(monthFrom) + 1)
+    .includes(dayjs(currentDate).month())
+}) as string
