bee35ff92e39ac78ab8de5e6938e2349683f3dff
João Vitor <jvitordiass@outlook.com>
2024-03-25 17:53:47 -0300

    fix: teams classification

--- src/shared/components/BabaLeaderboard/functions.ts
+++ src/shared/components/BabaLeaderboard/functions.ts
@@ -1,5 +1,5 @@
 import { TTeam } from '@/shared/types'
 
 export function sortTeamsByScore(teams: TTeam[]) {
-  return teams.sort((a, b) => b.wins * 3 + b.draws - a.wins * 3 + a.draws)
+  return teams.sort((a, b) => b.wins * 3 + b.draws - (a.wins * 3 + a.draws))
 }
