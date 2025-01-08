689e1f2b3340e9f22812360d25cc94696e2b31df
João Vitor <jvitordiass@outlook.com>
2025-01-08 18:01:39 -0300

    refactor: define `open`  as an optional prop

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,12 @@
 import { Backdrop, CircularProgress } from '@mui/material'
 
-export function Loader({ open }: { open: boolean }) {
+export function Loader({ open }: { open?: boolean }) {
   return (
     <Backdrop
       sx={{
         zIndex: (theme) => theme.zIndex.drawer + 1
       }}
-      open={open}
+      open={!!open}
     >
       <CircularProgress />
     </Backdrop>
