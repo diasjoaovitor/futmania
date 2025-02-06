ca2d02b6b7241dfa6ffcaa5a9e325b0e326cb369
João Vitor <jvitordiass@outlook.com>
2025-02-05 23:15:45 -0300

    🧪 test: simplify Jest configuration and setup
    - Remove collectCoverage and collectCoverageFrom settings from jest.config.ts
    - Remove console.error mocking from jest.setup.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,6 @@
 export default {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts(x)?',
-    '!src/*.tsx',
-    '!src/**/index.ts',
-    '!src/**/style.ts',
-    '!src/**/use*.ts',
-    '!src/**/nav-items.ts',
-    '!src/**/inputs.ts',
-    '!src/icons/*',
-    '!src/themes/*',
-    '!src/firebase/*',
-    '!src/states/*',
-    '!src/functions/getters/*'
-  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
--- jest.setup.ts
+++ jest.setup.ts
@@ -3,13 +3,3 @@ import '@testing-library/jest-dom'
 import { enableFetchMocks } from 'jest-fetch-mock'
 
 enableFetchMocks()
-
-const original = console.error
-
-beforeEach(() => {
-  console.error = jest.fn()
-})
-
-afterEach(() => {
-  console.error = original
-})
