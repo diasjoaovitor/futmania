3a335af24ca734e172743cbad1e83bb6d979c279
João Vitor <jvitordiass@outlook.com>
2025-02-09 11:05:03 -0300

    📦 refactor: remove all test files

--- src/components/Logo/logo.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Logo } from '.'
-
-describe('<Logo />', () => {
-  it('should render the app logo', () => {
-    render(<Logo />)
-    expect(screen.getByText('Futmania')).toBeInTheDocument()
-  })
-})
