67fb7559c11a9301baa006dd2669fdb645dee991
João Vitor <jvitordiass@outlook.com>
2025-02-16 13:38:23 -0300

    🔧 ci: reorder linting and formatting steps
    - Swap the order of the Lint and Prettier steps, now running Prettier before Lint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,8 +3,8 @@ const path = require('path')
 const buildCommand = (filenames) => {
   const files = filenames.map((f) => path.relative(process.cwd(), f))
   return [
-    `npx prettier --write ${files.join(' --file ')}`,
     `npx eslint --fix ${files.join(' ')}`,
+    `npx prettier --write ${files.join(' --file ')}`,
     `npx jest --runInBand --findRelatedTests ${files.join(' ')} --passWithNoTests`
   ]
 }
